b37ca598f71eda1142862d1466127059d1bd610c
Ken Chan <chankiyu22@gmail.com>
2024-12-18 16:49:13 +0800

    Init like NFT project
    refs LIK3-8

---
+++ likenft/README.md
@@ -0,0 +1,16 @@
+# Like NFT
+
+## Dev
+
+1. Start local dev node
+
+```bash
+$ npm run dev
+```
+
+2. Lint
+
+```bash
+$ npm run format
+$ npm run lint [-- --fix]
+```
---
+++ likenft/hardhat.config.ts
@@ -0,0 +1,44 @@
+import type { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox-viem";
+
+import "@nomicfoundation/hardhat-ethers";
+import "@openzeppelin/hardhat-upgrades";
+import dotenv from "dotenv";
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+        details: {
+          yulDetails: {
+            optimizerSteps: "u",
+          },
+        },
+      },
+      viaIR: true,
+    },
+  },
+
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      accounts: [`0x${process.env.DEPLOY_WALLET_PRIVATE_KEY_LOCALHOST}`],
+    },
+    sepolia: {
+      url: "https://ethereum-sepolia-rpc.publicnode.com",
+      chainId: 11155111,
+      accounts: [`0x${process.env.DEPLOY_WALLET_PRIVATE_KEY_OP_SEPOLIA}`],
+    },
+    "optimism-sepolia": {
+      url: "https://sepolia.optimism.io",
+      chainId: 11155420,
+      accounts: [`0x${process.env.DEPLOY_WALLET_PRIVATE_KEY_OP_SEPOLIA}`],
+    },
+  },
+};
+
+export default config;
