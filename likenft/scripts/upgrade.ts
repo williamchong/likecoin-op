f8c9059f9f9b6f4e6f2969f4331dfbb4d476ff95
Ken Chan <chankiyu22@gmail.com>
2025-01-14 16:09:38 +0800

    likenft: Verify source code when deploy and upgrade

--- likenft/scripts/upgrade.ts
+++ likenft/scripts/upgrade.ts
@@ -1,18 +1,49 @@
 import "@openzeppelin/hardhat-upgrades";
-import { ethers, upgrades } from "hardhat";
+import { ContractAlreadyVerifiedError } from "@nomicfoundation/hardhat-verify/internal/errors";
+import hardhat, { ethers, upgrades } from "hardhat";
 
 async function main() {
   // We get the contract to deploy
   const LikeNFT = await ethers.getContractFactory("LikeNFT");
   console.log("Upgrading LikeNFT...");
-  const likeNFT = await upgrades.upgradeProxy(
+
+  const newImplementationAddress = await upgrades.prepareUpgrade(
     process.env.PROXY_ADDRESS!,
     LikeNFT,
     {
       timeout: 0,
+      verifySourceCode: true,
+      kind: "uups",
     },
   );
-  console.log("LikeNFT deployed to:", await likeNFT.getAddress());
+
+  console.log(
+    "LikeNFT new implementation is deployed to:",
+    newImplementationAddress,
+  );
+
+  try {
+    await hardhat.run("verify:verify", {
+      address: newImplementationAddress,
+    });
+  } catch (e) {
+    if (e instanceof ContractAlreadyVerifiedError) {
+      // There may be the same implementation contract verified due to code revert
+      console.log(
+        "LikeNFT new implementation is already verified:",
+        newImplementationAddress,
+      );
+    } else {
+      throw e;
+    }
+  }
+
+  // TODO: Prepare an upgrade proposal to safe
+  const likeNFT = LikeNFT.attach(process.env.PROXY_ADDRESS!);
+  await likeNFT.upgradeToAndCall(newImplementationAddress, "0x");
+
+  console.log("LikeNFT upgraded implementation to:", newImplementationAddress);
+  console.log("LikeNFT proxy address is:", await likeNFT.getAddress());
 }
 
 main()
