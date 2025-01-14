dbaafea2ffa20c7ef6cac717c873803897ef7f70
Ken Chan <chankiyu22@gmail.com>
2025-01-14 15:54:17 +0800

    likecoin: Verify source code when deploy and upgrade

--- likecoin/scripts/upgrade.ts
+++ likecoin/scripts/upgrade.ts
@@ -1,18 +1,49 @@
 import "@openzeppelin/hardhat-upgrades";
-import { ethers, upgrades } from "hardhat";
+import { ContractAlreadyVerifiedError } from "@nomicfoundation/hardhat-verify/internal/errors";
+import hardhat, { ethers, upgrades } from "hardhat";
 
 async function main() {
   // We get the contract to deploy
   const EkilCoin = await ethers.getContractFactory("EkilCoin");
   console.log("Upgrading EkilCoin...");
-  const ekilCoin = await upgrades.upgradeProxy(
+
+  const newImplementationAddress = await upgrades.prepareUpgrade(
     process.env.PROXY_ADDRESS!,
     EkilCoin,
     {
       timeout: 0,
+      verifySourceCode: true,
+      kind: "uups",
     },
   );
-  console.log("EkilCoin deployed to:", await ekilCoin.getAddress());
+
+  console.log(
+    "EkilCoin new implementation is deployed to:",
+    newImplementationAddress,
+  );
+
+  try {
+    await hardhat.run("verify:verify", {
+      address: newImplementationAddress,
+    });
+  } catch (e) {
+    if (e instanceof ContractAlreadyVerifiedError) {
+      // There may be the same implementation contract verified due to code revert
+      console.log(
+        "LikeNFT new implementation is already verified:",
+        newImplementationAddress,
+      );
+    } else {
+      throw e;
+    }
+  }
+
+  // TODO: Prepare an upgrade proposal to safe
+  const ekilCoin = EkilCoin.attach(process.env.PROXY_ADDRESS!);
+  await ekilCoin.upgradeToAndCall(newImplementationAddress, "0x");
+
+  console.log("EkilCoin upgraded implementation to:", newImplementationAddress);
+  console.log("EkilCoin proxy address is:", await ekilCoin.getAddress());
 }
 
 main()
