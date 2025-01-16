7f3bbba7a112ace09b7200abdbf9411224f0d877
Ken Chan <chankiyu22@gmail.com>
2025-01-16 11:51:35 +0800

    likenft: Remove cosmos ref comments

--- likenft/types/Class.sol
+++ likenft/types/Class.sol
@@ -4,17 +4,6 @@ pragma solidity ^0.8.9;
 import {ClassConfig} from "./ClassConfig.sol";
 import {ClassParent} from "./ClassParent.sol";
 
-/**
- * Class {
- *   string id          = 1;
- *   string name        = 2;
- *   string symbol      = 3;
- *   string description = 4;
- *   string uri         = 5;
- *   string uri_hash    = 6;
- *   google.protobuf.Any data = 7;
- * }
- */
 struct ClassStorage {
     string id;
     string name;
@@ -25,17 +14,6 @@ struct ClassStorage {
     ClassDataStorage data;
 }
 
-/**
- * message ClassData {
- *   bytes metadata = 1 [
- *     (gogoproto.nullable) = false,
- *     (gogoproto.customtype) = "JsonInput"
- *   ];
- *   ClassParent parent = 2 [(gogoproto.nullable) = false];
- *   ClassConfig config = 3 [(gogoproto.nullable) = false];
- *   BlindBoxState blind_box_state = 4 [(gogoproto.nullable) = false];
- * }
- */
 struct ClassDataStorage {
     string metadata;
     ClassParent parent;
