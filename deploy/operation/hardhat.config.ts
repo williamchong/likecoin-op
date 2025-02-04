bc5b7a40151ab92fcf22fe990e1f3c36eae82eb6
Rick Mak <rick.mak@gmail.com>
2025-02-04 13:53:37 +0000

    Add operation scripts folder

---
+++ deploy/operation/hardhat.config.ts
@@ -0,0 +1,48 @@
+import "@nomicfoundation/hardhat-toolbox-viem";
+import type { HardhatUserConfig } from "hardhat/config";
+
+import "@nomicfoundation/hardhat-ethers";
+import "@nomicfoundation/hardhat-verify";
+import dotenv from "dotenv";
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.28",
+  etherscan: {
+    apiKey: {
+      "optimism-sepolia":
+        "Is not required by blockscout. Can be any non-empty string",
+    },
+    customChains: [
+      {
+        network: "optimism-sepolia",
+        chainId: 11155420,
+        urls: {
+          apiURL: "https://optimism-sepolia.blockscout.com/api",
+          browserURL: "https://optimism-sepolia.blockscout.com/",
+        },
+      },
+    ],
+  },
+  networks: {
+    ...(process.env.DEPLOY_WALLET_PRIVATE_KEY_LOCALHOST != null
+      ? {
+          localhost: {
+            url: "http://127.0.0.1:8545",
+            accounts: [`0x${process.env.DEPLOY_WALLET_PRIVATE_KEY_LOCALHOST}`],
+          },
+        }
+      : {}),
+    ...(process.env.DEPLOY_WALLET_PRIVATE_KEY_OP_SEPOLIA != null
+      ? {
+          "optimism-sepolia": {
+            url: "https://sepolia.optimism.io",
+            chainId: 11155420,
+            accounts: [`0x${process.env.DEPLOY_WALLET_PRIVATE_KEY_OP_SEPOLIA}`],
+          },
+        }
+      : {}),
+  },
+};
+
+export default config;
