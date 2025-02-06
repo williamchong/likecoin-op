2407cfddef94d720355178b620169f42355cc752
Ken Chan <chankiyu22@gmail.com>
2025-02-06 15:27:33 +0800

    likenft-migration: Add api client

--- likenft-migration/nuxt.config.ts
+++ likenft-migration/nuxt.config.ts
@@ -28,7 +28,11 @@ export default {
   i18n,
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [{ src: '~/plugins/textEncoder' }, '~/plugins/config.ts'],
+  plugins: [
+    { src: '~/plugins/textEncoder' },
+    '~/plugins/config.ts',
+    '~/plugins/apiClient.ts',
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
