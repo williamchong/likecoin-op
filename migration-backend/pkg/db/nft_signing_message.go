129c8d349d79884f3c7c5573f99c7648062dea9e
Ken Chan <chankiyu22@gmail.com>
2025-02-11 14:40:36 +0800

    migration-backend: Add query nft signing message by cosmos address

--- migration-backend/pkg/db/nft_signing_message.go
+++ migration-backend/pkg/db/nft_signing_message.go
@@ -6,6 +6,42 @@ import (
 	"github.com/likecoin/like-migration-backend/pkg/model"
 )
 
+func QueryNFTSigningMessageByCosmosAddress(
+	tx TxLike,
+	cosmosAddress string,
+) (*model.NFTSigningMessage, error) {
+	row := tx.QueryRow(
+		`SELECT
+	id,
+	created_at,
+	cosmos_address,
+	liker_id,
+	eth_address,
+	nonce,
+	issue_time,
+	message
+FROM nft_signing_message WHERE cosmos_address = $1`, cosmosAddress)
+
+	nftSigningMessage := &model.NFTSigningMessage{}
+
+	err := row.Scan(
+		&nftSigningMessage.Id,
+		&nftSigningMessage.CreatedAt,
+		&nftSigningMessage.CosmosAddress,
+		&nftSigningMessage.LikerID,
+		&nftSigningMessage.EthAddress,
+		&nftSigningMessage.Nonce,
+		&nftSigningMessage.IssueTime,
+		&nftSigningMessage.Message,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return nftSigningMessage, nil
+}
+
 func InsertNFTSigningMessage(
 	tx TxLike,
 	nftSigningMessage *model.NFTSigningMessage,
