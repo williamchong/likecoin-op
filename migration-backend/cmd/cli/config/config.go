ba9383f42b3649dc5cfdcc94ffe3ef3a9b1419a9
Ken Chan <chankiyu22@gmail.com>
2025-02-19 18:49:20 +0800

    migration-backend: Update cli env config

--- migration-backend/cmd/cli/config/config.go
+++ migration-backend/cmd/cli/config/config.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"math/big"
+
 	"github.com/kelseyhightower/envconfig"
 )
 
@@ -9,11 +11,14 @@ type contextKey struct{}
 var ContextKey = &contextKey{}
 
 type EnvConfig struct {
-	CosmosNodeUrl          string `envconfig:"COSMOS_NODE_URL"`
-	EthWalletPrivateKey    string `envconfig:"ETH_WALLET_PRIVATE_KEY"`
-	EthNetworkPublicRPCURL string `envconfig:"ETH_NETWORK_PUBLIC_RPC_URL"`
-	EthTokenAddress        string `envconfig:"ETH_TOKEN_ADDRESS"`
-	DbConnectionStr        string `envconfig:"DB_CONNECTION_STR"`
+	CosmosNodeUrl             string   `envconfig:"COSMOS_NODE_URL"`
+	EthWalletPrivateKey       string   `envconfig:"ETH_WALLET_PRIVATE_KEY"`
+	EthNetworkPublicRPCURL    string   `envconfig:"ETH_NETWORK_PUBLIC_RPC_URL"`
+	EthTokenAddress           string   `envconfig:"ETH_TOKEN_ADDRESS"`
+	EthChainId                *big.Int `envconfig:"ETH_CHAIN_ID"`
+	EthLikeNFTContractAddress string   `envconfig:"ETH_LIKENFT_CONTRACT_ADDRESS"`
+	DbConnectionStr           string   `envconfig:"DB_CONNECTION_STR"`
+	LikerlandUrlBase          string   `envconfig:"LIKERLAND_URL_BASE"`
 }
 
 func LoadEnvConfigFromEnv() (*EnvConfig, error) {
