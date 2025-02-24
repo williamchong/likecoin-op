4848bdb85d338b8aacef491eb3996df1b9e2f7e9
Ken Chan <chankiyu22@gmail.com>
2025-02-24 12:56:14 +0800

    migration-backend: Add nft migration clis

---
+++ migration-backend/cmd/cli/cmd/likenft/migrate_class.go
@@ -0,0 +1,89 @@
+package likenft
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/likecoin/like-migration-backend/cmd/cli/config"
+	"github.com/likecoin/like-migration-backend/pkg/likenft/cosmos"
+	"github.com/likecoin/like-migration-backend/pkg/likenft/evm"
+	"github.com/likecoin/like-migration-backend/pkg/logic/likenft"
+	"github.com/spf13/cobra"
+)
+
+var runLikeNFTMigrationPlanNewClassCmd = &cobra.Command{
+	Use:   "migrate-class likenft-asset-migration-class-id",
+	Short: "Mint NFT Class",
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) != 1 {
+			cmd.Usage()
+			return
+		}
+
+		envCfg := cmd.Context().Value(config.ContextKey).(*config.EnvConfig)
+		db, err := sql.Open("postgres", envCfg.DbConnectionStr)
+		if err != nil {
+			panic(err)
+		}
+
+		likenftClient := &cosmos.LikeNFTCosmosClient{
+			HTTPClient: &http.Client{
+				Timeout: 5 * time.Second,
+			},
+			NodeURL: envCfg.CosmosNodeUrl,
+		}
+
+		ethClient, err := ethclient.Dial(envCfg.EthNetworkPublicRPCURL)
+		if err != nil {
+			panic(err)
+		}
+		privateKey, err := crypto.HexToECDSA(envCfg.EthWalletPrivateKey)
+		if err != nil {
+			panic(err)
+		}
+		contractAddress := common.HexToAddress(envCfg.EthLikeNFTContractAddress)
+
+		evmLikeNFTClient := evm.NewLikeProtocol(
+			ethClient,
+			privateKey,
+			envCfg.EthChainId,
+			contractAddress,
+		)
+		evmLikeNFTClassClient := evm.NewLikeNFTClass(
+			ethClient,
+			privateKey,
+			envCfg.EthChainId,
+		)
+
+		idStr := args[0]
+		id, err := strconv.ParseUint(idStr, 10, 64)
+		if err != nil {
+			panic(err)
+		}
+
+		mc, err := likenft.MigrateClassFromAssetMigration(
+			db,
+			likenftClient,
+			&evmLikeNFTClient,
+			&evmLikeNFTClassClient,
+			envCfg.InitialNewClassOwner,
+			envCfg.InitialNewClassMinter,
+			envCfg.InitialNewClassUpdater,
+			id,
+		)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("migrate class completed, evm tx hash: %v", *mc.EvmTxHash)
+	},
+}
+
+func init() {
+	LikeNFTCmd.AddCommand(runLikeNFTMigrationPlanNewClassCmd)
+}
