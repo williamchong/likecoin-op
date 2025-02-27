2be23082ca56f880203abb4e538369b364953a40
Ken Chan <chankiyu22@gmail.com>
2025-02-27 18:30:53 +0800

    migration-backend: Add get class id from new class transaction helper

--- migration-backend/pkg/likenft/evm/new_class.go
+++ migration-backend/pkg/likenft/evm/new_class.go
@@ -1,8 +1,10 @@
 package evm
 
 import (
+	"errors"
 	"fmt"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/likecoin/like-migration-backend/pkg/likenft/evm/like_protocol"
 )
@@ -23,4 +25,21 @@ func (l *LikeProtocol) NewClass(msgNewClass like_protocol.MsgNewClass) (*types.T
 		return nil, fmt.Errorf("err instance.NewClass: %v", err)
 	}
 	return tx, nil
+}
+
+func (l *LikeProtocol) GetClassIdFromNewClassTransaction(txReceipt *types.Receipt) (*common.Address, error) {
+	filterer, err := like_protocol.NewLikeProtocolFilterer(l.ContractAddress, l.Client)
+	if err != nil {
+		return nil, fmt.Errorf("err likenft.NewLikenftFilterer: %v", err)
+	}
+
+	logs := txReceipt.Logs
+
+	for _, vLog := range logs {
+		newClassEvent, err := filterer.ParseNewClass(*vLog)
+		if err == nil {
+			return &newClassEvent.ClassId, nil
+		}
+	}
+	return nil, errors.New("err finding new class event from tx log")
 }
