a8863df58376d1e3d85014595c324f530cd79a78
Ken Chan <chankiyu22@gmail.com>
2025-02-27 18:41:03 +0800

    migration-backend: Do not specify transaction options

--- migration-backend/pkg/likenft/evm/like_protocol.go
+++ migration-backend/pkg/likenft/evm/like_protocol.go
@@ -1,14 +1,11 @@
 package evm
 
 import (
-	"context"
 	"crypto/ecdsa"
-	"fmt"
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
@@ -33,46 +30,10 @@ func NewLikeProtocol(
 	}
 }
 
-func (l *LikeProtocol) pubKey() (*ecdsa.PublicKey, error) {
-	publicKey := l.PrivateKey.Public()
-	pubKey, ok := publicKey.(*ecdsa.PublicKey)
-	if !ok {
-		return nil, fmt.Errorf("error casting public key to ECDSA")
-	}
-	return pubKey, nil
-}
-
-func (l *LikeProtocol) nonce() (*uint64, error) {
-	pubKey, err := l.pubKey()
-	if err != nil {
-		return nil, err
-	}
-	fromAddress := crypto.PubkeyToAddress(*pubKey)
-	nonce, err := l.Client.PendingNonceAt(context.Background(), fromAddress)
-	if err != nil {
-		return nil, err
-	}
-	return &nonce, nil
-}
-
 func (l *LikeProtocol) transactOpts() (*bind.TransactOpts, error) {
 	txOpts, err := bind.NewKeyedTransactorWithChainID(l.PrivateKey, l.ChainID)
 	if err != nil {
 		return nil, err
 	}
-	gasPrice, err := l.Client.SuggestGasPrice(context.Background())
-	if err != nil {
-		return nil, err
-	}
-	nonce, err := l.nonce()
-	if err != nil {
-		return nil, err
-	}
-
-	txOpts.Nonce = big.NewInt(int64(*nonce))
-	txOpts.Value = big.NewInt(0)       // in wei
-	txOpts.GasLimit = uint64(30000000) // in units
-	txOpts.GasPrice = gasPrice
-
 	return txOpts, nil
 }
