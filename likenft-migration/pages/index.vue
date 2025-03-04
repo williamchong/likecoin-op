f7495737637d0ba9bf73c0cb722c21b1c30439df
Ken Chan <chankiyu22@gmail.com>
2025-03-04 16:46:17 +0800

    likenft-migration: Show introduction in home page

--- likenft-migration/pages/index.vue
+++ likenft-migration/pages/index.vue
@@ -3,7 +3,12 @@
     <div :class="['-mb-[65px]']">
       <HeroBanner>
         <h1
-          :class="['text-3xl', 'font-semibold', 'text-likecoin-votecolor-yes']"
+          :class="[
+            'text-3xl',
+            'font-inter',
+            'font-semibold',
+            'text-likecoin-votecolor-yes',
+          ]"
         >
           {{ $t('app.title') }}
         </h1>
@@ -11,7 +16,16 @@
     </div>
     <div :class="['relative', 'z-10', 'max-w-[880px]', 'px-4', 'mx-auto']">
       <div :class="['bg-white', 'p-[30px]', 'rounded-md', 'shadow-md']">
-        <StepSection :step="1" :current-step="currentStep"></StepSection>
+        <StepSection :step="1" :current-step="currentStep">
+          <h2 :class="['text-base', 'font-semibold', 'text-likecoin-darkgrey']">
+            {{ $t('section.introduction.title') }}
+          </h2>
+          <div :class="['mt-2', 'mb-5']">
+            <SectionIntroduction
+              @confirmClicked="handleIntroductionSectionConfirmClick"
+            />
+          </div>
+        </StepSection>
         <StepSection :step="2" :current-step="currentStep"></StepSection>
         <StepSection :step="3" :current-step="currentStep"></StepSection>
         <StepSection :step="4" :current-step="currentStep"></StepSection>
@@ -27,6 +41,11 @@ export default Vue.extend({
   computed: {
     currentStep() {
       return 1;
+    },
+  },
+  methods: {
+    handleIntroductionSectionConfirmClick() {
+      alert('TODO');
     },
   },
 });
