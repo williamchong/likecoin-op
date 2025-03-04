5a246cd7b4095fe5fcddc57c24aa2561803220d2
Ken Chan <chankiyu22@gmail.com>
2025-03-04 16:40:08 +0800

    likenft-migration: Implement skeleton

--- likenft-migration/pages/index.vue
+++ likenft-migration/pages/index.vue
@@ -1,11 +1,33 @@
 <template>
-  <div
-    class="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"
-  >
-    <main class="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-      <primary-button to="/migrate">{{
-        $t('index.get-started')
-      }}</primary-button>
-    </main>
+  <div :class="['flex-1', 'min-h-0', 'bg-likecoin-lightergrey']">
+    <div :class="['-mb-[65px]']">
+      <HeroBanner>
+        <h1
+          :class="['text-3xl', 'font-semibold', 'text-likecoin-votecolor-yes']"
+        >
+          {{ $t('app.title') }}
+        </h1>
+      </HeroBanner>
+    </div>
+    <div :class="['relative', 'z-10', 'max-w-[880px]', 'px-4', 'mx-auto']">
+      <div :class="['bg-white', 'p-[30px]', 'rounded-md', 'shadow-md']">
+        <StepSection :step="1" :current-step="currentStep"></StepSection>
+        <StepSection :step="2" :current-step="currentStep"></StepSection>
+        <StepSection :step="3" :current-step="currentStep"></StepSection>
+        <StepSection :step="4" :current-step="currentStep"></StepSection>
+      </div>
+    </div>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  computed: {
+    currentStep() {
+      return 1;
+    },
+  },
+});
+</script>
