e20247eaca9c61138d345e42b5ad74b0cbafbf61
Ken Chan <chankiyu22@gmail.com>
2025-03-18 22:18:10 +0800

    likecoin-migration: Add cosmos and evm explorer base url config

--- likecoin-migration/models/config.ts
+++ likecoin-migration/models/config.ts
@@ -4,6 +4,8 @@ export const ConfigSchema = z.object({
   isTestnet: z.boolean().catch(true),
   apiBaseURL: z.string(),
   cosmosDepositAddress: z.string(),
+  cosmosExplorerBaseURL: z.string(),
+  evmExplorerBaseURL: z.string(),
 });
 
 export type Config = z.infer<typeof ConfigSchema>;
