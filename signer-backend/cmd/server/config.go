e8dd3bc319a40fd358a8c7bdd6e37508d9f4eb75
Ken Chan <chankiyu22@gmail.com>
2025-03-21 17:07:37 +0800

    signer-backend: Add evm client config

--- signer-backend/cmd/server/config.go
+++ signer-backend/cmd/server/config.go
@@ -5,10 +5,12 @@ import (
 )
 
 type EnvConfig struct {
-	ListenAddr      string `envconfig:"LISTEN_ADDR" default:"0.0.0.0:8091"`
-	DbConnectionStr string `envconfig:"DB_CONNECTION_STR"`
-	ApiKey          string `envconfig:"API_KEY"`
-	RoutePrefix     string `envconfig:"ROUTE_PREFIX" default:""`
+	ListenAddr             string `envconfig:"LISTEN_ADDR" default:"0.0.0.0:8091"`
+	DbConnectionStr        string `envconfig:"DB_CONNECTION_STR"`
+	ApiKey                 string `envconfig:"API_KEY"`
+	RoutePrefix            string `envconfig:"ROUTE_PREFIX" default:""`
+	EvmSignerPrivateKey    string `envconfig:"EVM_SIGNER_PRIVATE_KEY"`
+	EvmNetworkPublicRpcUrl string `envconfig:"ETH_NETWORK_PUBLIC_RPC_URL"`
 }
 
 func LoadEnvConfigFromEnv() (*EnvConfig, error) {
