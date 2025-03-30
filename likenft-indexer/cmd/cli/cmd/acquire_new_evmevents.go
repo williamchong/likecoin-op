d00f54626d70a7491a08ed128ac47081d5789516
Ken Chan <chankiyu22@gmail.com>
2025-03-30 18:04:02 +0800

    likenft-indexer: Add acquire new evmevents cli

---
+++ likenft-indexer/cmd/cli/cmd/acquire_new_evmevents.go
@@ -0,0 +1,53 @@
+package cmd
+
+import (
+	"likenft-indexer/cmd/cli/context"
+	"likenft-indexer/ent/evmeventprocessedblockheight"
+	"likenft-indexer/internal/database"
+	"likenft-indexer/internal/evm"
+	"likenft-indexer/internal/logic/evmeventacquirer"
+
+	"github.com/spf13/cobra"
+)
+
+// make cli acquire-new-evm-events TransferWithMemo '0x14CE6632272552E676b53FE6202edA8F1Be4992c'
+var AcquireNewEVMEvents = &cobra.Command{
+	Use:   "acquire-new-evm-events event class-id",
+	Short: "Acquire new events",
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) != 2 {
+			_ = cmd.Usage()
+			return
+		}
+
+		eventRaw := args[0]
+		event := evmeventprocessedblockheight.Event(eventRaw)
+		classIdStr := args[1]
+
+		ctx := cmd.Context()
+		cfg := context.ConfigFromContext(ctx)
+		logger := context.LoggerFromContext(ctx)
+
+		dbService := database.New()
+		evmClient, err := evm.NewEvmClient(cfg.EthNetworkPublicRPCURL)
+
+		if err != nil {
+			panic(err)
+		}
+
+		EVMEventProcessedBlockHeightRepository := database.MakeEVMEventProcessedBlockHeightRepository(dbService)
+		EVMEventRepository := database.MakeEVMEventRepository(dbService)
+
+		acquirer := evmeventacquirer.MakeEvmEventsAcquirer(
+			EVMEventProcessedBlockHeightRepository,
+			EVMEventRepository,
+			evmClient,
+		)
+
+		err = acquirer.Acquire(ctx, logger, classIdStr, event)
+
+		if err != nil {
+			panic(err)
+		}
+	},
+}
--- likenft-indexer/cmd/cli/cmd/root.go
+++ likenft-indexer/cmd/cli/cmd/root.go
@@ -32,4 +32,5 @@ func Execute(
 }
 
 func init() {
+	rootCmd.AddCommand(AcquireNewEVMEvents)
 }
