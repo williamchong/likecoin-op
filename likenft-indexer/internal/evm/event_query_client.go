fda54d3c7af5a259d3fc53a8f58f3961a8632484
Ken Chan <chankiyu22@gmail.com>
2025-04-03 12:05:01 +0800

    likenft-indexer: Add evm event query client

---
+++ likenft-indexer/internal/evm/event_query_client.go
@@ -0,0 +1,77 @@
+package evm
+
+import (
+	"context"
+
+	"likenft-indexer/internal/evm/book_nft"
+	"likenft-indexer/internal/evm/like_protocol"
+
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+type EVMQueryClient interface {
+	GetLikeProtocolABI() *abi.ABI
+	GetBookNFTABI() *abi.ABI
+
+	QueryBookNFTOwnershipTransferred(
+		ctx context.Context,
+		contractAddress common.Address,
+		startBlock uint64,
+	) ([]types.Log, error)
+	QueryContractURIUpdated(
+		ctx context.Context,
+		contractAddress common.Address,
+		startBlock uint64,
+	) ([]types.Log, error)
+	QueryNewBookNFT(
+		ctx context.Context,
+		contractAddress common.Address,
+		startBlock uint64,
+	) ([]types.Log, error)
+	QueryTransferWithMemo(
+		ctx context.Context,
+		contractAddress common.Address,
+		startBlock uint64,
+	) ([]types.Log, error)
+}
+
+type evmQueryClient struct {
+	client          *ethclient.Client
+	LikeProtocolABI *abi.ABI
+	BookNFTABI      *abi.ABI
+}
+
+func NewEvmQueryClient(url string) (EVMQueryClient, error) {
+	client, err := ethclient.Dial(url)
+	if err != nil {
+		return nil, err
+	}
+
+	likeprotocolABI, err := like_protocol.LikeProtocolMetaData.GetAbi()
+	if err != nil {
+		return nil, err
+	}
+
+	booknftABI, err := book_nft.BookNftMetaData.GetAbi()
+	if err != nil {
+		return nil, err
+	}
+
+	return &evmQueryClient{
+		client:          client,
+		LikeProtocolABI: likeprotocolABI,
+		BookNFTABI:      booknftABI,
+	}, nil
+}
+
+func (c *evmQueryClient) GetLikeProtocolABI() *abi.ABI {
+	return c.LikeProtocolABI
+}
+
+func (c *evmQueryClient) GetBookNFTABI() *abi.ABI {
+	return c.BookNFTABI
+
+}
