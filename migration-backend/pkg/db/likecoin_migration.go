6934e6445a6f921d8c17ea0b8e119a49a2c14522
Rico Wong <hochiw@oursky.com>
2025-04-08 14:12:24 +0800

    migration-backend: Add likecoin migration paginated queries
    refs LIK3-179

--- migration-backend/pkg/db/likecoin_migration.go
+++ migration-backend/pkg/db/likecoin_migration.go
@@ -56,6 +56,81 @@ WHERE user_cosmos_address = $1 AND status NOT IN ($2, $3)
 	return m, nil
 }
 
+func QueryPaginatedLikeCoinMigration(
+	tx TxLike,
+	limit int,
+	offset int,
+	status *model.LikeCoinMigrationStatus,
+	keyword string,
+) ([]*model.LikeCoinMigration, error) {
+	rows, err := tx.Query(`SELECT
+    id,
+    created_at,
+    user_cosmos_address,
+    burning_cosmos_address,
+    minting_eth_address,
+    user_eth_address,
+    amount,
+    evm_signature,
+    evm_signature_message,
+    status,
+    cosmos_tx_hash,
+    evm_tx_hash,
+    failed_reason
+FROM likecoin_migration
+WHERE ($3::text IS NULL OR status = $3) AND
+(
+	$4::text = '' OR 
+	failed_reason ILIKE '%' || $4 || '%' OR 
+	user_cosmos_address ILIKE '%' || $4 || '%' OR 
+	burning_cosmos_address ILIKE '%' || $4 || '%' OR 
+	minting_eth_address ILIKE '%' || $4 || '%' OR 
+	user_eth_address ILIKE '%' || $4 || '%' OR
+	evm_signature ILIKE '%' || $4 || '%' OR 
+	evm_signature_message ILIKE '%' || $4 || '%' OR 
+	cosmos_tx_hash ILIKE '%' || $4 || '%' OR 
+	evm_tx_hash ILIKE '%' || $4 || '%'
+)
+ORDER BY created_at DESC
+LIMIT $1
+OFFSET $2
+
+`, limit, offset, status, keyword)
+
+	if err != nil {
+		return nil, err
+	}
+
+	migrations := []*model.LikeCoinMigration{}
+
+	for rows.Next() {
+		m := &model.LikeCoinMigration{}
+		err := rows.Scan(
+			&m.Id,
+			&m.CreatedAt,
+			&m.UserCosmosAddress,
+			&m.BurningCosmosAddress,
+			&m.MintingEthAddress,
+			&m.UserEthAddress,
+			&m.Amount,
+			&m.EvmSignature,
+			&m.EvmSignatureMessage,
+			&m.Status,
+			&m.CosmosTxHash,
+			&m.EvmTxHash,
+			&m.FailedReason,
+		)
+
+		if err != nil {
+			return nil, err
+		}
+
+		migrations = append(migrations, m)
+	}
+
+	return migrations, nil
+}
+
 func QueryLatestLikeCoinMigration(
 	tx TxLike,
 	cosmosAddress string,
