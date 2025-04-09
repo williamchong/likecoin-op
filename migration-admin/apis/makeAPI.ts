bb564863dfa37d4c37ce34db2adfc1f6a6ad21f1
Rico Wong <hochiw@oursky.com>
2025-04-09 12:01:25 +0800

    migration-admin: Update makeAPI function to support query params
    refs LIK3-135 LIK3-138

--- migration-admin/apis/makeAPI.ts
+++ migration-admin/apis/makeAPI.ts
@@ -1,7 +1,19 @@
 import type { NuxtAxiosInstance } from "@nuxtjs/axios";
-import { Method } from "axios";
-import { z } from "zod";
+import { AxiosRequestConfig, Method } from "axios";
+import { z, ZodTypeDef } from "zod";
 
+export function makeAPI<Resp, Req = void>(def: {
+  url: string;
+  method: "GET" | "get";
+  requestSchema?: z.ZodSchema<URLSearchParams, ZodTypeDef, Req>;
+  responseSchema: z.ZodSchema<Resp>;
+}): (axios: NuxtAxiosInstance) => (req: Req) => Promise<Resp>;
+export function makeAPI<Resp, Req = void>(def: {
+  url: string;
+  method: "DELETE" | "delete";
+  requestSchema?: z.ZodSchema<URLSearchParams, ZodTypeDef, Req>;
+  responseSchema: z.ZodSchema<Resp>;
+}): (axios: NuxtAxiosInstance) => (req: Req) => Promise<Resp>;
 export function makeAPI<Resp, Req = void>(def: {
   url: string;
   method: Method;
@@ -9,10 +21,21 @@ export function makeAPI<Resp, Req = void>(def: {
   responseSchema: z.ZodSchema<Resp>;
 }): (axios: NuxtAxiosInstance) => (req: Req) => Promise<Resp> {
   return (axios: NuxtAxiosInstance) => async (req: Req) => {
+    const request = def.requestSchema ? def.requestSchema.parse(req) : req;
+    const requestParams: Pick<
+      Partial<AxiosRequestConfig>,
+      "data" | "params"
+    > = def.method === "GET" || def.method === "get"
+      ? {
+          params: request,
+        }
+      : {
+          data: request,
+        };
     const resp = await axios.$request({
       method: def.method,
       url: def.url,
-      data: req,
+      ...requestParams,
     });
     const response = def.responseSchema.parse(resp);
     return response;
