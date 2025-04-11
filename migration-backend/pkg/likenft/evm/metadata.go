c36ea32ff9aff9602fe0e009e49ae0b99eae93a7
Ken Chan <chankiyu22@gmail.com>
2025-04-11 17:34:16 +0800

    migration-backend: Remove unused metadata conversion

--- migration-backend/pkg/likenft/evm/metadata.go
+++ migration-backend/pkg/likenft/evm/metadata.go
@@ -10,18 +10,6 @@ import (
 	evmmodel "github.com/likecoin/like-migration-backend/pkg/likenft/evm/model"
 )
 
-func ContractLevelMetadataFromCosmosClass(c *cosmosmodel.Class) *evmmodel.ContractLevelMetadata {
-	return &evmmodel.ContractLevelMetadata{
-		ContractLevelMetadataOpenSea: evmmodel.ContractLevelMetadataOpenSea{
-			Name:         c.Name,
-			Symbol:       c.Symbol,
-			Description:  c.Description,
-			Image:        c.Data.Metadata.Image,
-			ExternalLink: c.Data.Metadata.ExternalURL,
-		},
-	}
-}
-
 func ContractLevelMetadataFromCosmosClassAndISCN(c *cosmosmodel.Class, iscn *cosmosmodel.ISCN) *evmmodel.ContractLevelMetadata {
 	iscnRecord := iscn.Records[0].Data
 
@@ -69,30 +57,6 @@ func ContractLevelMetadataFromCosmosClassAndISCN(c *cosmosmodel.Class, iscn *cos
 		LikeCoin: &evmmodel.ContractLevelMetadataLikeCoin{
 			ISCNIdPrefix: c.Data.Parent.IscnIdPrefix,
 			ClassId:      c.Id,
-		},
-	}
-}
-
-func ContractLevelMetadataFromCosmosClassListItem(c *cosmosmodel.ClassListItem) *evmmodel.ContractLevelMetadata {
-	return &evmmodel.ContractLevelMetadata{
-		ContractLevelMetadataOpenSea: evmmodel.ContractLevelMetadataOpenSea{
-			Name:         c.Name,
-			Symbol:       c.Symbol,
-			Description:  c.Description,
-			Image:        c.Metadata.Image,
-			ExternalLink: c.Metadata.ExternalURL,
-		},
-	}
-}
-
-func ERC721MetadataFromCosmosNFT(c *cosmosmodel.NFT) *evmmodel.ERC721Metadata {
-	return &evmmodel.ERC721Metadata{
-		ERC721MetadataOpenSea: evmmodel.ERC721MetadataOpenSea{
-			Image:       c.Data.Metadata.Image,
-			ExternalUrl: c.Data.Metadata.ExternalUrl,
-			Description: c.Data.Metadata.Description,
-			Name:        c.Data.Metadata.Name,
-			Attributes:  sortERC721MetadataAttributes(makeERC721MetadataAttribute("", c.Data.Metadata.Attributes)),
 		},
 	}
 }
