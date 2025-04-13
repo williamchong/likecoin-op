163bb61bc4922509f5e2d6183b14b791a916e0a8
Ken Chan <chankiyu22@gmail.com>
2025-04-13 12:45:12 +0800

    Revert "likecoin-migration, likenft-migration: Prompt axios error"
    This reverts commit ebd36530097f24d532971ccddd18d4088e5db651.

--- likenft-migration/apis/makeAPI.ts
+++ likenft-migration/apis/makeAPI.ts
@@ -9,17 +9,12 @@ export function makeAPI<Resp, Req = void>(def: {
   responseSchema: z.ZodSchema<Resp>;
 }): (axios: NuxtAxiosInstance) => (req: Req) => Promise<Resp> {
   return (axios: NuxtAxiosInstance) => async (req: Req) => {
-    try {
-      const resp = await axios.$request({
-        method: def.method,
-        url: def.url,
-        data: req,
-      });
-      const response = def.responseSchema.parse(resp);
-      return response;
-    } catch (err) {
-      alert('Unexpected error happened. Please try again later. \n\n' + err);
-      throw err;
-    }
+    const resp = await axios.$request({
+      method: def.method,
+      url: def.url,
+      data: req,
+    });
+    const response = def.responseSchema.parse(resp);
+    return response;
   };
 }
