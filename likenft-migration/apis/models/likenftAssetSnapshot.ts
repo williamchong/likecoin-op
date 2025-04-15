a5b0098404d2102493ab398822308990959fa9de
Ken Chan <chankiyu22@gmail.com>
2025-04-15 13:47:33 +0800

    likenft-migration: Introduce empty and non empty snapshot models

--- likenft-migration/apis/models/likenftAssetSnapshot.ts
+++ likenft-migration/apis/models/likenftAssetSnapshot.ts
@@ -34,7 +34,7 @@ export const LikeNFTAssetSnapshotStatusSchema = z.enum([
   'failed',
 ]);
 
-export const LikeNFTAssetSnapshotSchema = z.object({
+const LikeNFTAssetSnapshotSchemaBase = z.object({
   id: z.number(),
   created_at: z.coerce.date(),
   cosmos_address: z.string(),
@@ -42,8 +42,48 @@ export const LikeNFTAssetSnapshotSchema = z.object({
   block_time: z.coerce.date().nullable(),
   status: LikeNFTAssetSnapshotStatusSchema,
   failed_reason: z.string().nullable(),
-  classes: z.array(LikeNFTAssetSnapshotClassSchema),
-  nfts: z.array(LikeNFTAssetSnapshotNFTSchema),
 });
 
+export const EmptyLikeNFTAssetSnapshotSchema =
+  LikeNFTAssetSnapshotSchemaBase.extend({
+    classes: z.tuple([]),
+    nfts: z.tuple([]),
+  });
+
+export type EmptyLikeNFTAssetSnapshot = z.infer<
+  typeof EmptyLikeNFTAssetSnapshotSchema
+>;
+
+export const NonEmptyLikeNFTAssetSnapshotSchema = z.union([
+  LikeNFTAssetSnapshotSchemaBase.extend({
+    classes: z.array(LikeNFTAssetSnapshotClassSchema).nonempty(),
+    nfts: z.array(LikeNFTAssetSnapshotNFTSchema),
+  }),
+  LikeNFTAssetSnapshotSchemaBase.extend({
+    classes: z.array(LikeNFTAssetSnapshotClassSchema),
+    nfts: z.array(LikeNFTAssetSnapshotNFTSchema).nonempty(),
+  }),
+]);
+
+export type NonEmptyLikeNFTAssetSnapshot = z.infer<
+  typeof NonEmptyLikeNFTAssetSnapshotSchema
+>;
+
+export const LikeNFTAssetSnapshotSchema = z.union([
+  EmptyLikeNFTAssetSnapshotSchema,
+  NonEmptyLikeNFTAssetSnapshotSchema,
+]);
+
 export type LikeNFTAssetSnapshot = z.infer<typeof LikeNFTAssetSnapshotSchema>;
+
+export function isEmptyLikeNFTAssetSnapshot(
+  s: LikeNFTAssetSnapshot
+): s is EmptyLikeNFTAssetSnapshot {
+  return s.nfts.length === 0 && s.classes.length === 0;
+}
+
+export function isNonEmptyLikeNFTAssetSnapshot(
+  s: LikeNFTAssetSnapshot
+): s is NonEmptyLikeNFTAssetSnapshot {
+  return !isEmptyLikeNFTAssetSnapshot(s);
+}
