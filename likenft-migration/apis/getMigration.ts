756833ca68aa7647da1a2c6edac26abeb68624c0
Ken Chan <chankiyu22@gmail.com>
2025-04-15 10:54:33 +0800

    likenft-migration: Add snapshot to get migration api

--- likenft-migration/apis/getMigration.ts
+++ likenft-migration/apis/getMigration.ts
@@ -2,9 +2,11 @@ import { z } from 'zod';
 
 import { makeAPI } from './makeAPI';
 import { LikeNFTAssetMigrationSchema } from './models/likenftAssetMigration';
+import { LikeNFTAssetSnapshotSchema } from './models/likenftAssetSnapshot';
 
 export const ResponseSchema = z.object({
   migration: LikeNFTAssetMigrationSchema,
+  snapshot: LikeNFTAssetSnapshotSchema,
 });
 
 export type Response = z.infer<typeof ResponseSchema>;
