c0c820ba8d022b6417974c22ecf717999f8a8327
Rick Mak <rick.mak@gmail.com>
2025-05-13 14:58:29 +0000

    likeprotocol: Update the example scripts

--- likenft/scripts/examples/mintNFT.ts
+++ likenft/scripts/examples/mintNFT.ts
@@ -1,91 +1,35 @@
 import { ethers } from "hardhat";
 
 async function mintNFT() {
-  const classId = "0xAf0f5e9a70349947961691DB04f642c480200315";
+  const classId = "0x1D146390C1D4E03C74b87D896b254a5468EDF804";
   const signer = await ethers.provider.getSigner();
 
-  const LikeProtocol = await ethers.getContractAt(
-    "LikeProtocol",
-    process.env.ERC721_PROXY_ADDRESS!,
-  );
+  const LikeNFTClass = await ethers.getContractAt("BookNFT", classId);
+  const likeNFTClass = LikeNFTClass.connect(signer);
 
-  const likeProtocol = LikeProtocol.connect(signer);
-
-  const tx = await likeProtocol.mintNFT({
-    to: signer.address,
-    classId: classId,
-    input: {
-      metadata: JSON.stringify({
-        image:
-          "ipfs://bafybeie2zfhndeavdc7ebbitr3krbxlxtjsdwmzbdkkkmiscg4j5xf3rwi",
+  const tx = await likeNFTClass.mint(
+    signer.address,
+    ["mint via script"],
+    [
+      JSON.stringify({
+        image: "ipfs://QmUEV41Hbi7qkxeYSVUtoE5xkfRFnqSd62fa5v8Naya5Ys",
         image_data: "",
-        external_url: "https://bit.ly/moneyverse-pdf",
-        description: "#0017",
-        name: "#0017",
+        external_url: "https://www.google.com",
+        description: "202412191729 #0001 Description",
+        name: "202412191729 #0001",
         attributes: [
           {
-            trait_type: "background",
-            value: "Fade Out",
-          },
-          {
-            trait_type: "publish_info_layout",
-            value: "Bottom",
-          },
-          {
-            trait_type: "coins_layout",
-            value: "Random",
-          },
-          {
-            trait_type: "coins_color",
-            value: "Sliver",
-          },
-          {
-            display_type: "number",
-            trait_type: "coin_1_rotate_x",
-            value: 0.0045,
-          },
-          {
-            display_type: "number",
-            trait_type: "coin_1_rotate_z",
-            value: 0.001,
-          },
-          {
-            display_type: "number",
-            trait_type: "coin_2_rotate_x",
-            value: 0.0052,
-          },
-          {
-            display_type: "number",
-            trait_type: "coin_2_rotate_z",
-            value: 0.0057,
-          },
-          {
-            display_type: "number",
-            trait_type: "coin_1_position_x",
-            value: 135,
-          },
-          {
-            display_type: "number",
-            trait_type: "coin_1_position_y",
-            value: 21,
-          },
-          {
-            display_type: "number",
-            trait_type: "coin_2_position_x",
-            value: -160,
-          },
-          {
-            display_type: "number",
-            trait_type: "coin_2_position_y",
-            value: 158,
+            trait_type: "ISCN ID",
+            value:
+              "iscn://likecoin-chain/FyZ13m_hgwzUC6UoaS3vFdYvdG6QXfajU3vcatw7X1c/1",
           },
         ],
         background_color: "",
         animation_url: "",
         youtube_url: "",
       }),
-    },
-  });
+    ],
+  );
   console.log(await tx.wait());
 }
 
