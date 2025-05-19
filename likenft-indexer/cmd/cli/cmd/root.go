40e90699b67ab578811cd11e755c86982f37f192
Ken Chan <chankiyu22@gmail.com>
2025-05-19 16:20:58 +0800

    likenft-indexer: Remove unused cli
    refs likecoin/likecoin-op#67

--- likenft-indexer/cmd/cli/cmd/root.go
+++ likenft-indexer/cmd/cli/cmd/root.go
@@ -37,10 +37,7 @@ func Execute(
 }
 
 func init() {
-	rootCmd.AddCommand(AcquireBookNFTs)
 	rootCmd.AddCommand(AcquireContractEvents)
-	rootCmd.AddCommand(AcquireNewEVMEvents)
-	rootCmd.AddCommand(AcquireAllBookNFTEvmEvents)
 	rootCmd.AddCommand(ProcessAllEVMEventCmd)
 	rootCmd.AddCommand(ProcessEVMEventCmd)
 	rootCmd.AddCommand(RecalculateEvmEventsDecodedParamsCmd)
