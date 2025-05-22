08dbc93a67b25a5877f3c52e862766b2cdee0b81
Ken Chan <chankiyu22@gmail.com>
2025-05-22 14:15:34 +0800

    likenft-indexer: Handler internal server error in openapi handlers

--- likenft-indexer/internal/api/openapi/error.go
+++ likenft-indexer/internal/api/openapi/error.go
@@ -4,10 +4,15 @@ import (
 	"context"
 
 	"likenft-indexer/ent"
+	"likenft-indexer/internal/api/openapi/model"
 	"likenft-indexer/openapi/api"
+
+	"github.com/getsentry/sentry-go"
 )
 
 func (h *OpenAPIHandler) NewError(ctx context.Context, err error) *api.ErrorStatusCode {
+	hub := sentry.GetHubFromContext(ctx)
+
 	if ent.IsNotFound(err) {
 		return &api.ErrorStatusCode{
 			StatusCode: 404,
@@ -17,11 +22,13 @@ func (h *OpenAPIHandler) NewError(ctx context.Context, err error) *api.ErrorStat
 			},
 		}
 	}
+
 	return &api.ErrorStatusCode{
 		StatusCode: 500,
 		Response: api.Error{
-			Code:    500,
-			Message: "internal server error",
+			Code:          500,
+			Message:       "internal server error",
+			SentryErrorID: model.MakeOptString((*string)(hub.CaptureException(err))),
 		},
 	}
 }
