856cb7239fccdd772aa4ad9c857dc08732f1ddaa
Ken Chan <chankiyu22@gmail.com>
2025-05-27 11:46:21 +0800

    likenft-migration: Add ipfs and ar http base url configs

--- likenft-migration/models/config.ts
+++ likenft-migration/models/config.ts
@@ -1,12 +1,21 @@
 import { z } from 'zod';
 
-export const ConfigSchema = z.object({
-  transitionLoadingScreenDelayMs: z.number().default(0),
-  isTestnet: z.boolean().catch(true),
-  authcoreRedirectUrl: z.string(),
-  apiBaseURL: z.string(),
-  likerlandUrlBase: z.string(),
-  crispWebsiteId: z.string(),
+export const ImageURLConfigSchema = z.object({
+  ipfsHTTPBaseURL: z.string().default('https://ipfs.io/ipfs'),
+  arHTTPBaseURL: z.string().default('https://arweave.net'),
 });
+
+export type ImageURLConfig = z.infer<typeof ImageURLConfigSchema>;
+
+export const ConfigSchema = z
+  .object({
+    transitionLoadingScreenDelayMs: z.number().default(0),
+    isTestnet: z.boolean().catch(true),
+    authcoreRedirectUrl: z.string(),
+    apiBaseURL: z.string(),
+    likerlandUrlBase: z.string(),
+    crispWebsiteId: z.string(),
+  })
+  .merge(ImageURLConfigSchema);
 
 export type Config = z.infer<typeof ConfigSchema>;
