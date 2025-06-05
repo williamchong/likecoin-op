851444ee8f7c272c4635955d37d84a2cf719e19c
Ken Chan <chankiyu22@gmail.com>
2025-06-05 13:44:03 +0800

    migration-backend: Add ERC721_METADATA_EXTERNAL_URL_BASE_3OOK config
    refs likecoin/likecoin-op#84

--- migration-backend/cmd/worker/config/config.go
+++ migration-backend/cmd/worker/config/config.go
@@ -22,11 +22,12 @@ type EnvConfig struct {
 	LikecoinAPIUrlBase              string `envconfig:"LIKECOIN_API_URL_BASE"`
 	LikecoinAPIHTTPTimeoutSeconds   int    `envconfig:"LIKECOIN_API_HTTP_TIMEOUT_SECONDS" default:"10"`
 
-	InitialNewClassOwner      string `envconfig:"INITIAL_NEW_CLASS_OWNER"`
-	InitialNewClassMinter     string `envconfig:"INITIAL_NEW_CLASS_MINTER"`
-	InitialNewClassUpdater    string `envconfig:"INITIAL_NEW_CLASS_UPDATER"`
-	InitialBatchMintNFTsOwner string `envconfig:"INITIAL_BATCH_MINT_NFTS_OWNER"`
-	BatchMintItemPerPage      uint64 `envconfig:"BATCH_MINT_ITEM_PER_PAGE"`
+	InitialNewClassOwner              string `envconfig:"INITIAL_NEW_CLASS_OWNER"`
+	InitialNewClassMinter             string `envconfig:"INITIAL_NEW_CLASS_MINTER"`
+	InitialNewClassUpdater            string `envconfig:"INITIAL_NEW_CLASS_UPDATER"`
+	InitialBatchMintNFTsOwner         string `envconfig:"INITIAL_BATCH_MINT_NFTS_OWNER"`
+	BatchMintItemPerPage              uint64 `envconfig:"BATCH_MINT_ITEM_PER_PAGE"`
+	ERC721MetadataExternalURLBase3ook string `envconfig:"ERC721_METADATA_EXTERNAL_URL_BASE_3OOK"`
 }
 
 func LoadEnvConfigFromEnv() (*EnvConfig, error) {
