f60211d194c55527531c36b624b62bc573869395
Ken Chan <chankiyu22@gmail.com>
2025-07-14 15:00:01 +0800

    migration-backend: Introduce not found in HandleResponseStatus util
    refs likecoin/likecoin-op#131

--- migration-backend/pkg/util/httputil/response.go
+++ migration-backend/pkg/util/httputil/response.go
@@ -5,10 +5,15 @@ import (
 	"net/http"
 )
 
+var ErrNotFound = errors.New("err not found")
+
 func HandleResponseStatus(
 	resp *http.Response,
 ) error {
 	if resp.StatusCode >= 400 {
+		if resp.StatusCode == 404 {
+			return errors.Join(ErrNotFound, errors.New(resp.Status))
+		}
 		return errors.New(resp.Status)
 	}
 	return nil
