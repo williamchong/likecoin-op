0dae2ca6c608e69b21d4b6eb0f1b0fff73d7d7c8
Ken Chan <chankiyu22@gmail.com>
2025-07-18 16:46:42 +0800

    likenft-indexer: Codegen make migration
    refs likecoin/likecoin-op#140

---
+++ likenft-indexer/ent/migrate/migrations/20250718084522_Add acquire_book_nft_events columns to nft_class table.sql
@@ -0,0 +1,12 @@
+-- Modify "nft_classes" table
+ALTER TABLE "nft_classes"
+    ADD COLUMN "acquire_book_nft_events_weight" double precision NOT NULL DEFAULT 1,
+    ADD COLUMN "acquire_book_nft_events_last_processed_time" timestamptz NULL,
+    ADD COLUMN "acquire_book_nft_events_score" numeric NULL,
+    ADD COLUMN "acquire_book_nft_events_status" character varying NULL,
+    ADD COLUMN "acquire_book_nft_events_failed_reason" character varying NULL,
+    ADD COLUMN "acquire_book_nft_events_failed_count" bigint NOT NULL;
+-- Create index "nftclass_acquire_book_nft_events_score" to table: "nft_classes"
+CREATE INDEX "nftclass_acquire_book_nft_events_score" ON "nft_classes" ("acquire_book_nft_events_score");
+-- Create index "nftclass_acquire_book_nft_events_status" to table: "nft_classes"
+CREATE INDEX "nftclass_acquire_book_nft_events_status" ON "nft_classes" ("acquire_book_nft_events_status");
