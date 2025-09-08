b70ea957f243f6df519373228b7e01a61242d354
Ken Chan <chankiyu22@gmail.com>
2025-09-08 16:31:37 +0800

    likecoin-migration: Add section error row component
    refs likecoin/likecoin-op#185

---
+++ likecoin-migration/components/SectionErrorRow.vue
@@ -0,0 +1,45 @@
+<template>
+  <div :class="['flex', 'items-center']">
+    <div :class="['flex-1']">
+      <p
+        v-if="errorMessage != null"
+        :class="['text-likecoin-votecolor-no', 'text-xs']"
+      >
+        <FontAwesomeIcon :class="['font-bold']" icon="triangle-exclamation" />
+        {{ errorMessage }}
+      </p>
+    </div>
+    <div v-if="!!$listeners['retryClick']">
+      <AppButton
+        variant="primary"
+        :class="['w-full']"
+        @click="handleRetryClick"
+      >
+        {{ retryButtonText ?? $t('section.error-row.retry') }}
+      </AppButton>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue';
+
+export default Vue.extend({
+  props: {
+    errorMessage: {
+      type: String as PropType<string>,
+      required: true,
+    },
+    retryButtonText: {
+      type: String as PropType<string>,
+      required: true,
+    },
+  },
+
+  methods: {
+    handleRetryClick() {
+      this.$emit('retryClick');
+    },
+  },
+});
+</script>
