f7496a54721001aba5770829d4b4c3f8075f2518
Ken Chan <chankiyu22@gmail.com>
2025-09-09 18:04:32 +0800

    migration-backend: Init logger with group dispatch slack handler

--- migration-backend/cmd/worker/main.go
+++ migration-backend/cmd/worker/main.go
@@ -8,22 +8,38 @@ import (
 	"github.com/go-redis/redis"
 	"github.com/hibiken/asynq"
 	"github.com/joho/godotenv"
+	slogslack "github.com/samber/slog-slack"
 
 	"github.com/likecoin/like-migration-backend/cmd/worker/cmd"
 	"github.com/likecoin/like-migration-backend/cmd/worker/config"
+	logginghandler "github.com/likecoin/like-migration-backend/pkg/logging/handler"
 )
 
 func main() {
-	logger := slog.New(slog.Default().Handler())
+	defaultHandler := slog.Default().Handler()
+	initLogger := slog.New(defaultHandler)
 	err := godotenv.Load()
 	if errors.Is(err, os.ErrNotExist) {
-		logger.Warn("skip loading .env as it is absent")
+		initLogger.Warn("skip loading .env as it is absent")
 	} else if err != nil {
 		panic(err)
 	}
 	envCfg, err := config.LoadEnvConfigFromEnv()
 	if err != nil {
 		panic(err)
+	}
+
+	logger := slog.New(defaultHandler)
+
+	if envCfg.LogLikecoinTxSlackWebhookUrl != "" && envCfg.LogLikecoinTxSlackChannel != "" {
+		slackHandler := slogslack.Option{
+			WebhookURL: envCfg.LogLikecoinTxSlackWebhookUrl,
+			Channel:    envCfg.LogLikecoinTxSlackChannel,
+		}.NewSlackHandler()
+
+		logger = slog.New(logginghandler.NewGroupDispatchHandler(map[string]slog.Handler{
+			envCfg.LogLikecoinTxSlackMatchGroup: slackHandler,
+		}, defaultHandler))
 	}
 
 	opt, err := redis.ParseURL(envCfg.RedisDsn)
