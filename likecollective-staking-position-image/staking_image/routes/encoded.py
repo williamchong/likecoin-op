00c1fa54e7b675ff4c3f395efb382fc8573723ac
Ken Chan <chankiyu22@gmail.com>
2025-10-17 14:31:33 +0800

    staking-image: Retrieve booknft name from rpc when request image
    refs likecoin/likecoin-op#250

--- likecollective-staking-position-image/staking_image/routes/encoded.py
+++ likecollective-staking-position-image/staking_image/routes/encoded.py
@@ -1,6 +1,12 @@
+import logging
+
 from fastapi import APIRouter, Response
 
-from staking_image.gen_image import gen_image_by_encoded_string
+from staking_image.deps import EVMClientDep
+from staking_image.gen_image import gen_image_by_params
+from staking_image.gen_image.params import parse_base64_encoded_params
+
+logger = logging.getLogger(__name__)
 
 router = APIRouter()
 
@@ -10,9 +16,18 @@ router = APIRouter()
     response_class=Response,
 )
 def get_image_from_encoded_string(
+    evm_client: EVMClientDep,
     encoded_string: str,
 ):
-    content = gen_image_by_encoded_string(encoded_string, format="PNG")
+    params = parse_base64_encoded_params(encoded_string)
+
+    if params.book_nft_name is None:
+        try:
+            params.book_nft_name = evm_client.get_likenft_name(params.book_nft_address)
+        except Exception as e:
+            logger.warning(f"Failed to get book NFT name: {e}")
+
+    content = gen_image_by_params(params, format="PNG")
 
     return Response(
         content=content,
--- likecollective-staking-position-image/staking_image/routes/params.py
+++ likecollective-staking-position-image/staking_image/routes/params.py
@@ -1,18 +1,30 @@
+import logging
+
 from fastapi import APIRouter, Response
 
+from staking_image.deps import EVMClientDep
 from staking_image.gen_image import Params, gen_image_by_params
+
+logger = logging.getLogger(__name__)
 
 router = APIRouter()
 
 
 @router.get("/", response_class=Response)
 def get_image_from_params(
+    evm_client: EVMClientDep,
     book_nft_address: str,
     staked_amount: str,
     reward_index: str,
     initial_staker: str,
     book_nft_name: str | None = None,
 ):
+    if book_nft_name is None:
+        try:
+            book_nft_name = evm_client.get_likenft_name(book_nft_address)
+        except Exception as e:
+            logger.warning(f"Failed to get book NFT name: {e}")
+
     content = gen_image_by_params(
         Params(
             book_nft_address=book_nft_address,
