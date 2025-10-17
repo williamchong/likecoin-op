4125ee3aa88af16b133e1cfd02daf78794532b5e
Ken Chan <chankiyu22@gmail.com>
2025-10-17 15:24:16 +0800

    staking-image: Add method lru cache util
    refs likecoin/likecoin-op#250

---
+++ likecollective-staking-position-image/staking_image/utils/methodtools/lru_cache.py
@@ -0,0 +1,18 @@
+import functools
+import weakref
+
+
+def lru_cache(maxsize=128, typed=False):
+    def wrapper(func):
+
+        @functools.lru_cache(maxsize, typed)
+        def _func(_self, *args, **kwargs):
+            return func(_self(), *args, **kwargs)
+
+        @functools.wraps(func)
+        def inner(self, *args, **kwargs):
+            return _func(weakref.ref(self), *args, **kwargs)
+
+        return inner
+
+    return wrapper
