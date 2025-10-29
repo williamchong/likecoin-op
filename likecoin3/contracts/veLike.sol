39b2a0912049c54ca3cbcfb7348efaf0de83b4a3
Rick Mak <rick.mak@gmail.com>
2025-10-29 12:46:08 +0000

    veLike: First draft of linear reward voting vault

--- likecoin3/contracts/veLike.sol
+++ likecoin3/contracts/veLike.sol
@@ -2,12 +2,14 @@
 pragma solidity ^0.8.27;
 
 import {ERC4626Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC4626Upgradeable.sol";
-import {IERC4626} from "@openzeppelin/contracts/interfaces/IERC4626.sol";
+import {ERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import {PausableUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
 import {ReentrancyGuardUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC4626} from "@openzeppelin/contracts/interfaces/IERC4626.sol";
 import {Likecoin} from "./Likecoin.sol";
 
 /// @custom:security-contact rickmak@oursky.com
@@ -18,6 +20,46 @@ contract veLike is
     PausableUpgradeable,
     ReentrancyGuardUpgradeable
 {
+    struct StakingCondition {
+        uint256 startTime;
+        uint256 endTime;
+        uint256 rewardAmount;
+        uint256 rewardIndex;
+    }
+
+    struct StakerInfo {
+        uint256 stakedAmount;
+        uint256 rewardIndex;
+        uint256 lockedUntil;
+        uint256 rewardClaimed; // Not use for calculation, only for tracking.
+    }
+
+    struct veLikeStorage {
+        uint256 rewardPool; // Tracking the likecoin hold by vault for reward distribution.
+        uint256 totalStaked;
+        uint256 lastRewardBlock;
+        uint256 currentConditionIndex;
+        mapping(address account => StakerInfo stakerInfo) stakerInfos;
+        mapping(uint256 index => StakingCondition condition) conditions;
+    }
+
+    uint256 public constant ACC_REWARD_PRECISION = 1e18; // Precision scalar for reward index
+
+    // keccak256(abi.encode(uint256(keccak256("veLike.storage")) - 1)) & ~bytes32(uint256(0xff))
+    bytes32 private constant CLASS_DATA_STORAGE =
+        0xb9e14b2a89d227541697d62a06ecbf5ccc9ad849800745b40b2826662a177600;
+
+    function _getveLikeData() private pure returns (veLikeStorage storage $) {
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+            $.slot := CLASS_DATA_STORAGE
+        }
+    }
+
+    // Errors
+    error ErrNoRewardToClaim();
+    error ErrConflictCondition();
+
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
@@ -36,144 +78,284 @@ contract veLike is
         address newImplementation
     ) internal override onlyOwner {}
 
-    /* Start of IERC20 interface */
-
-    /* End of IERC20 interface */
-
     /**
-     * @dev Returns the total amount of the underlying asset managed by the vault.
+     * totalAssets function
+     *
+     * veLike to Like should be one to one mapping, so the total supply is equal to the total assets.
+     * Note: Vault share is not veLike.
      */
     function totalAssets() public view override returns (uint256) {
-        return totalAssets();
+        return totalSupply();
     }
 
+    // Start of veLike specific functions
+
     /**
-     * @dev Returns the current amount of shares that `assets` would mint.
+     * getCurrentCondition function
+     *
+     * Get the current staking condition, it can be inactive. i.e. not started or alreadyended.
+     *
+     * @return currentCondition - the current staking condition
      */
-    function convertToShares(
-        uint256 assets
-    ) public view override returns (uint256) {
-        return convertToShares(assets);
+    function getCurrentCondition()
+        public
+        view
+        returns (StakingCondition memory)
+    {
+        StakingCondition memory currentCondition;
+        bool _isActive;
+        (currentCondition, _isActive) = _activeCondition();
+        return currentCondition;
     }
 
     /**
-     * @dev Returns the current amount of assets that `shares` would redeem.
+     * getPendingReward function
+     *
+     * Get the pending reward for the account.
+     *
+     * @param account - the account to get the pending reward for
+     * @return pendingReward - the pending reward for the account
      */
-    function convertToAssets(
-        uint256 shares
-    ) public view override returns (uint256) {
-        return convertToAssets(shares);
+    function getPendingReward(address account) public view returns (uint256) {
+        veLikeStorage storage $ = _getveLikeData();
+        StakingCondition memory currentCondition = $.conditions[
+            $.currentConditionIndex
+        ];
+        StakerInfo memory stakerInfo = $.stakerInfos[account];
+        return
+            (stakerInfo.stakedAmount *
+                (currentCondition.rewardIndex - stakerInfo.rewardIndex)) /
+            ACC_REWARD_PRECISION;
     }
 
     /**
-     * @dev Returns the maximum amount of assets that `receiver` can deposit right now.
+     * _claimReward function
+     *
+     * Internal function to update the vault rewardIndex and rewardIndex for the staker.
+     *
+     * @param condition - the staking condition
+     * @param stakerInfo - the staker info
+     * @param pendingReward - the pending reward for the account
      */
-    function maxDeposit(
-        address receiver
-    ) public view override returns (uint256) {
-        return maxDeposit(receiver);
+    function _claimReward(
+        StakingCondition memory condition,
+        StakerInfo memory stakerInfo,
+        uint256 pendingReward
+    ) internal {
+        veLikeStorage storage $ = _getveLikeData();
+        _updateVault();
+        stakerInfo.rewardClaimed += pendingReward;
+        stakerInfo.rewardIndex = condition.rewardIndex;
+        $.rewardPool -= pendingReward;
     }
 
     /**
-     * @dev Allows an on-chain or off-chain user to simulate the effects of their deposit at the current block, given current on-chain conditions.
+     * claimReward function
+     *
+     * Claim the reward for the account.
+     *
+     * @param account - the account to claim the reward for
+     * @return reward - the reward for the account
      */
-    function previewDeposit(
-        uint256 assets
-    ) public view override returns (uint256) {
-        return previewDeposit(assets);
+    function claimReward(
+        address account
+    ) public whenNotPaused nonReentrant returns (uint256) {
+        veLikeStorage storage $ = _getveLikeData();
+
+        uint256 pendingReward = getPendingReward(account);
+        if (pendingReward == 0) {
+            revert ErrNoRewardToClaim();
+        }
+        _claimReward(
+            $.conditions[$.currentConditionIndex],
+            $.stakerInfos[account],
+            pendingReward
+        );
+        SafeERC20.safeTransfer(IERC20(asset()), account, pendingReward);
+        return pendingReward;
     }
 
     /**
-     * @dev Mints shares Vault shares to receiver by depositing exactly amount of underlying tokens.
+     * restakeReward function
+     *
+     * Restake the reward for the account.
+     *
+     * @param account - the account to restake the reward
+     * @return reward - the amount of asset restaked
      */
-    function deposit(
-        uint256 assets,
-        address receiver
-    ) public override returns (uint256 shares) {
-        return deposit(assets, receiver);
+    function restakeReward(
+        address account
+    ) public nonReentrant returns (uint256) {
+        veLikeStorage storage $ = _getveLikeData();
+
+        uint256 pendingReward = getPendingReward(account);
+        if (pendingReward == 0) {
+            revert ErrNoRewardToClaim();
+        }
+        _claimReward(
+            $.conditions[$.currentConditionIndex],
+            $.stakerInfos[account],
+            pendingReward
+        );
+        $.stakerInfos[account].stakedAmount += pendingReward;
+        return pendingReward;
     }
 
+    function _activeCondition() internal view returns (StakingCondition memory, bool) {
+        veLikeStorage storage $ = _getveLikeData();
+        StakingCondition memory currentCondition = $.conditions[$.currentConditionIndex];
+        if (block.number < currentCondition.startTime || block.number > currentCondition.endTime) {
+            return (currentCondition, false);
+        }
+        return (currentCondition, true);
+    }
     /**
-  @dev Returns the maximum amount of the Vault shares that can be minted for the receiver, through a mint call.
-
-
-**/
-    function maxMint(address receiver) public view override returns (uint256) {
-        return maxMint(receiver);
+     * _updateVault function
+     *
+     * Update the vault reward index and reward debt.
+     *
+     */
+    function _updateVault() internal {
+        veLikeStorage storage $ = _getveLikeData();
+        StakingCondition memory currentCondition;
+        bool _isActive;
+        (currentCondition, _isActive) = _activeCondition();
+        uint256 targetBlock = block.number;
+        if (!_isActive) {
+            // Perform last update if needed
+            if ($.lastRewardBlock > currentCondition.startTime && $.lastRewardBlock < currentCondition.endTime) {
+                targetBlock = currentCondition.endTime;
+            } else {
+                return;
+            }
+        }
+        if ($.totalStaked > 0) {
+            uint256 blocksPassed = targetBlock - $.lastRewardBlock;
+            uint256 reward = blocksPassed *
+                _rewardPerBlockWithPrecision(currentCondition);
+            currentCondition.rewardIndex += reward / $.totalStaked;
+            $.lastRewardBlock = targetBlock;
+        }
     }
 
-    /**
-  @dev Allows an on-chain or off-chain user to simulate the effects of their mint at the current block, given current on-chain conditions.
-**/
-    function previewMint(
-        uint256 shares
-    ) public view override returns (uint256) {
-        return previewMint(shares);
+    function _rewardPerBlockWithPrecision(
+        StakingCondition memory condition
+    ) internal pure returns (uint256) {
+        return
+            (ACC_REWARD_PRECISION * condition.rewardAmount) /
+            (condition.endTime - condition.startTime);
     }
 
-    /**
-  @dev Mints exactly shares Vault shares to receiver by depositing amount of underlying tokens.
-**/
-    function mint(
-        uint256 shares,
-        address receiver
-    ) public override returns (uint256 assets) {
-        return mint(shares, receiver);
-    }
+    // End of veLike specific functions
+
+    // Start of ERC4626 Overrides
 
     /**
-  @dev Returns the maximum amount of the underlying asset that can be withdrawn from the owner balance in the Vault, through a withdraw call.
-**/
-    function maxWithdraw(address owner) public view override returns (uint256) {
-        return maxWithdraw(owner);
-    }
-
-    /**
-  @dev Allows an on-chain or off-chain user to simulate the effects of their withdraw at the current block, given current on-chain conditions.
-**/
-    function previewWithdraw(
-        uint256 assets
-    ) public view override returns (uint256) {
-        return previewWithdraw(assets);
-    }
-
-    /**
-  @dev Burns shares from owner and sends exactly assets of underlying tokens to receiver.
-**/
-    function withdraw(
-        uint256 assets,
+     * _deposit function
+     *
+     * Override ERC4626 _deposit function to update staker info on vault share. mint
+     *
+     * @param caller - the caller of the deposit
+     * @param receiver - the receiver of the vault share
+     * @param assets - the amount of asset to deposit
+     * @param shares - the amount of shares to mint
+     */
+    function _deposit(
+        address caller,
         address receiver,
-        address owner
-    ) public override returns (uint256 shares) {
-        return withdraw(assets, receiver, owner);
-    }
-
-    /**
-  @dev Returns the maximum amount of Vault shares that can be redeemed from the owner balance in the Vault, through a redeem call.
-**/
-    function maxRedeem(address owner) public view override returns (uint256) {
-        return maxRedeem(owner);
-    }
-
-    /**
-  @dev Allows an on-chain or off-chain user to simulate the effects of their redemption at the current block, given current on-chain conditions.
-**/
-    function previewRedeem(
+        uint256 assets,
         uint256 shares
-    ) public view override returns (uint256) {
-        return previewRedeem(shares);
+    ) internal virtual override {
+        veLikeStorage storage $ = _getveLikeData();
+        // Copying from ERC4626 _deposit function for clarity
+        SafeERC20.safeTransferFrom(
+            IERC20(asset()),
+            caller,
+            address(this),
+            assets
+        );
+        _mint(receiver, shares);
+
+        // Vault specific logic
+        _updateVault();
+        $.stakerInfos[receiver].stakedAmount += assets;
+
+        // Copying from ERC4626 _deposit function Event for clarity
+        emit Deposit(caller, receiver, assets, shares);
     }
 
     /**
-  @dev Burns exactly shares from owner and sends assets of underlying tokens to receiver.
-
-
-**/
-    function redeem(
-        uint256 shares,
+     * _withdraw function
+     *
+     * Override ERC4626 _withdraw function to update staker info on vault share. burn
+     *
+     * @param caller - the caller of the withdraw
+     * @param receiver - the receiver of the vault share
+     * @param assets - the amount of asset to withdraw
+     * @param shares - the amount of shares to burn
+     */
+    function _withdraw(
+        address caller,
         address receiver,
-        address owner
-    ) public override returns (uint256 assets) {
-        return redeem(shares, receiver, owner);
+        address owner,
+        uint256 assets,
+        uint256 shares
+    ) internal virtual override {
+        // Copying from ERC4626 _withdraw function for clarity
+        // Same as calling super._withdraw(caller, receiver, assets, shares);
+        if (caller != owner) {
+            _spendAllowance(owner, caller, shares);
+        }
+
+        _burn(owner, shares);
+        SafeERC20.safeTransfer(IERC20(asset()), receiver, assets);
+
+        // Vault specific logic
+        _updateVault();
+
+        // Copying from ERC4626 _withdraw function Event for clarity
+        emit Withdraw(caller, receiver, owner, assets, shares);
     }
+    // End of ERC4626 Overrides
+
+    // Start of Admin functions
+
+    /**
+     * addReward function
+     *
+     * Admin function for authorized address too deposit asset as reward. This
+     * staking vault rewards is linearly over time. reward calculation is update in the current block timestamp.
+     *
+     * @param rewardAmount - the amount of reward to deposit, asset ERC20(likecoin)
+     * @param endTime - the end time of the staking condition
+     */
+    function addReward(
+        uint256 rewardAmount,
+        uint256 startTime,
+        uint256 endTime
+    ) external onlyOwner {
+        veLikeStorage storage $ = _getveLikeData();
+        if ($.currentConditionIndex != 0) {
+            revert ErrConflictCondition();
+        }
+        SafeERC20.safeTransferFrom(
+            IERC20(asset()),
+            _msgSender(),
+            address(this),
+            rewardAmount
+        );
+        // perform last update if needed
+        _updateVault();
+        uint256 lastRewardIndex = $.conditions[$.currentConditionIndex].rewardIndex;
+        $.rewardPool += rewardAmount;
+        $.currentConditionIndex++;
+        $.conditions[$.currentConditionIndex] = StakingCondition({
+            startTime: startTime,
+            endTime: endTime,
+            rewardAmount: rewardAmount,
+            rewardIndex: lastRewardIndex
+        });
+    }
+
+    // End of Admin functions
 }
--- likecoin3/test/veLike.ts
+++ likecoin3/test/veLike.ts
@@ -7,8 +7,9 @@ import { viem, ignition } from "hardhat";
 
 import "./setup";
 import veLikeModule from "../ignition/modules/veLike";
+import { deployProtocol } from "./factory";
 
-describe("veLike", async function () {
+describe("veLike ", async function () {
   async function deployVeLike() {
     const [deployer, rick, kin, bob] = await viem.getWalletClients();
     const publicClient = await viem.getPublicClient();
@@ -39,40 +40,193 @@ describe("veLike", async function () {
       publicClient,
     };
   }
-
-  it("should have the correct owner", async function () {
-    const { veLike, deployer } = await loadFixture(deployVeLike);
-    expect(await veLike.read.owner()).to.equalAddress(deployer.account.address);
-  });
-
-  it("should have the correct asset address", async function () {
-    const { veLike, likecoin } = await loadFixture(deployVeLike);
-    expect(await veLike.read.asset()).to.equalAddress(likecoin.address);
-  });
-
-  it("should have the correct name and symbol", async function () {
-    const { veLike } = await loadFixture(deployVeLike);
-    expect(await veLike.read.name()).to.equal("vote-escrowed LikeCoin");
-    expect(await veLike.read.symbol()).to.equal("veLIKE");
-  });
-
-  it("should have the correct decimals", async function () {
-    const { veLike } = await loadFixture(deployVeLike);
-    expect(await veLike.read.decimals()).to.equal(6);
-  });
-
-  it("should have the correct STORAGE_SLOT", async function () {
-    const { veLike, deployer } = await loadFixture(deployVeLike);
-    const veLikeMock = await viem.deployContract("veLikeMock");
-    veLike.write.upgradeTo(veLikeMock.address, {
-      account: deployer.account,
+  async function initialMint() {
+    const { veLike, likecoin, deployer, rick, kin, bob, publicClient } =
+      await loadFixture(deployVeLike);
+    await likecoin.write.mint([deployer.account.address, 50000n * 10n ** 6n], {
+      account: deployer.account.address,
     });
-    const newVeLike = await viem.getContractAt(
-      "veLikeMock",
-      veLikeMock.address,
-    );
-    expect(await newVeLike.read.dataStorage()).to.equal(
-      "0xb9e14b2a89d227541697d62a06ecbf5ccc9ad849800745b40b2826662a177600",
-    );
+    await likecoin.write.mint([rick.account.address, 10000n * 10n ** 6n], {
+      account: deployer.account.address,
+    });
+    await likecoin.write.mint([kin.account.address, 10000n * 10n ** 6n], {
+      account: deployer.account.address,
+    });
+    await likecoin.write.mint([bob.account.address, 10000n * 10n ** 6n], {
+      account: deployer.account.address,
+    });
+    return { veLike, likecoin, deployer, rick, kin, bob, publicClient };
+  }
+  describe("as ERC1967 Proxy", async function () {
+    it("should have the correct owner", async function () {
+      const { veLike, deployer } = await loadFixture(deployVeLike);
+      expect(await veLike.read.owner()).to.equalAddress(
+        deployer.account.address,
+      );
+    });
+
+    it("should have the correct asset address", async function () {
+      const { veLike, likecoin } = await loadFixture(deployVeLike);
+      expect(await veLike.read.asset()).to.equalAddress(likecoin.address);
+    });
+
+    it("should have the correct name and symbol", async function () {
+      const { veLike } = await loadFixture(deployVeLike);
+      expect(await veLike.read.name()).to.equal("vote-escrowed LikeCoin");
+      expect(await veLike.read.symbol()).to.equal("veLIKE");
+    });
+
+    it("should have the correct decimals", async function () {
+      const { veLike } = await loadFixture(deployVeLike);
+      expect(await veLike.read.decimals()).to.equal(6);
+    });
+
+    it("should have the correct STORAGE_SLOT", async function () {
+      const { veLike, deployer } = await loadFixture(deployVeLike);
+      const veLikeMock = await viem.deployContract("veLikeMock");
+      veLike.write.upgradeTo(veLikeMock.address, {
+        account: deployer.account,
+      });
+      const newVeLike = await viem.getContractAt(
+        "veLikeMock",
+        veLikeMock.address,
+      );
+      expect(await newVeLike.read.version()).to.equal(2n);
+      expect(await newVeLike.read.dataStorage()).to.equal(
+        "0xb9e14b2a89d227541697d62a06ecbf5ccc9ad849800745b40b2826662a177600",
+      );
+    });
   });
+
+  describe("as ERC20", async function () {
+    it("should have the zero initial supply supply", async function () {
+      const { veLike } = await loadFixture(initialMint);
+      expect(await veLike.read.totalSupply()).to.equal(0n);
+    });
+  });
+
+  describe("as ERC4626 vault", async function () {
+    it("should have increase supply after deposit", async function () {
+      const { veLike, likecoin, deployer, rick } =
+        await loadFixture(initialMint);
+      await likecoin.write.approve([veLike.address, 100n * 10n ** 6n], {
+        account: rick.account.address,
+      });
+      await veLike.write.deposit([100n * 10n ** 6n, rick.account.address], {
+        account: rick.account.address,
+      });
+      expect(await veLike.read.balanceOf([rick.account.address])).to.equal(
+        100n * 10n ** 6n,
+      );
+      expect(await veLike.read.totalAssets()).to.equal(100n * 10n ** 6n);
+      expect(await veLike.read.totalSupply()).to.equal(100n * 10n ** 6n);
+      expect(await likecoin.read.balanceOf([rick.account.address])).to.equal(
+        9900n * 10n ** 6n,
+      );
+    });
+
+    it("should have increase supply after multiple deposit", async function () {
+      const { veLike, likecoin, deployer, rick, kin } =
+        await loadFixture(initialMint);
+      await likecoin.write.approve([veLike.address, 100n * 10n ** 6n], {
+        account: rick.account.address,
+      });
+      await veLike.write.deposit([100n * 10n ** 6n, rick.account.address], {
+        account: rick.account.address,
+      });
+      expect(await veLike.read.balanceOf([rick.account.address])).to.equal(
+        100n * 10n ** 6n,
+      );
+      expect(await likecoin.read.balanceOf([rick.account.address])).to.equal(
+        9900n * 10n ** 6n,
+      );
+
+      await likecoin.write.approve([veLike.address, 200n * 10n ** 6n], {
+        account: kin.account.address,
+      });
+      await veLike.write.deposit([200n * 10n ** 6n, kin.account.address], {
+        account: kin.account.address,
+      });
+      expect(await veLike.read.balanceOf([kin.account.address])).to.equal(
+        200n * 10n ** 6n,
+      );
+      expect(await likecoin.read.balanceOf([kin.account.address])).to.equal(
+        9800n * 10n ** 6n,
+      );
+
+      expect(await veLike.read.totalAssets()).to.equal(300n * 10n ** 6n);
+      expect(await veLike.read.totalSupply()).to.equal(300n * 10n ** 6n);
+    });
+
+    it("should keep total assets as zero after adding reward", async function () {
+      const { veLike, likecoin, deployer, publicClient } =
+        await loadFixture(initialMint);
+      await likecoin.write.approve([veLike.address, 10000n * 10n ** 6n], {
+        account: deployer.account.address,
+      });
+      const block = await publicClient.getBlock();
+      const endTime = block.timestamp + 1000n;
+      await veLike.write.addReward([10000n * 10n ** 6n, endTime], {
+        account: deployer.account.address,
+      });
+      expect(await veLike.read.totalAssets()).to.equal(0n);
+    });
+
+    it("should have increase supply after deposit and add reward", async function () {
+      const { veLike, likecoin, deployer, rick, publicClient } =
+        await loadFixture(initialMint);
+      await likecoin.write.approve([veLike.address, 100n * 10n ** 6n], {
+        account: rick.account.address,
+      });
+      await veLike.write.deposit([100n * 10n ** 6n, rick.account.address], {
+        account: rick.account.address,
+      });
+      expect(await veLike.read.balanceOf([rick.account.address])).to.equal(
+        100n * 10n ** 6n,
+      );
+      expect(await veLike.read.totalAssets()).to.equal(100n * 10n ** 6n);
+      expect(await veLike.read.totalSupply()).to.equal(100n * 10n ** 6n);
+      expect(await likecoin.read.balanceOf([rick.account.address])).to.equal(
+        9900n * 10n ** 6n,
+      );
+
+      const block = await publicClient.getBlock();
+      const endTime = block.timestamp + 1000n;
+      await likecoin.write.approve([veLike.address, 10000n * 10n ** 6n], {
+        account: deployer.account.address,
+      });
+      await veLike.write.addReward([10000n * 10n ** 6n, endTime], {
+        account: deployer.account.address,
+      });
+      expect(await veLike.read.totalAssets()).to.equal(100n * 10n ** 6n);
+    });
+  });
+
+  describe("reward distribution on empty account", async function () {
+    it("should have return zero on account never deposited", async function () {
+      const { veLike, rick } = await loadFixture(initialMint);
+      expect(
+        await veLike.read.getPendingReward([rick.account.address]),
+      ).to.equal(0n);
+    });
+
+    it("should be revert on claimReward on empty account", async function () {
+      const { veLike, rick } = await loadFixture(initialMint);
+      await expect(
+        veLike.write.claimReward([rick.account.address], {
+          account: rick.account.address,
+        }),
+      ).to.be.rejectedWith("ErrNoRewardToClaim");
+    });
+
+    it("should be revert on restakeReward on empty account", async function () {
+      const { veLike, rick } = await loadFixture(initialMint);
+      await expect(
+        veLike.write.restakeReward([rick.account.address], {
+          account: rick.account.address,
+        }),
+      ).to.be.rejectedWith("ErrNoRewardToClaim");
+    });
+  });
+
 });
