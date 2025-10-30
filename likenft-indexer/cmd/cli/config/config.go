404f70f25a7c7ce0e80cd05a5cd050b84f583a09
Ken Chan <chankiyu22@gmail.com>
2025-10-30 16:27:56 +0800

    likenft-indexer: Add EVM_EVENT_QUERY_TO_BLOCK_PADDING env config
    refs likecoin/likecoin-op#254

--- likenft-indexer/cmd/cli/config/config.go
+++ likenft-indexer/cmd/cli/config/config.go
@@ -12,6 +12,8 @@ type EnvConfig struct {
 	EthNetworkEventRPCURL          string `envconfig:"ETH_NETWORK_EVENT_RPC_URL"`
 	EthNetworkPublicRPCURL         string `envconfig:"ETH_NETWORK_PUBLIC_RPC_URL"`
 	EthLikeProtocolContractAddress string `envconfig:"ETH_LIKE_PROTOCOL_CONTRACT_ADDRESS"`
+
+	EvmEventQueryToBlockPadding uint64 `envconfig:"EVM_EVENT_QUERY_TO_BLOCK_PADDING" default:"10"`
 }
 
 func LoadEnvConfigFromEnv() (*EnvConfig, error) {
