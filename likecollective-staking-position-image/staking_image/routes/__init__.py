591168f3245f305a6cac1c1ff6d30e8b47e5d9e8
Ken Chan <chankiyu22@gmail.com>
2025-11-04 16:20:27 +0800

    staking-image: Move image generation api to /image
    refs likecoin/likecoin-op#262

--- likecollective-staking-position-image/staking_image/routes/__init__.py
+++ likecollective-staking-position-image/staking_image/routes/__init__.py
@@ -1,11 +1,9 @@
 from fastapi import APIRouter
 
-from .encoded import router as encoded_router
 from .health import router as health_router
-from .params import router as params_router
+from .image import router as image_router
 
 router = APIRouter(tags=["image"])
 
 router.include_router(health_router)
-router.include_router(params_router)
-router.include_router(encoded_router)
+router.include_router(image_router)
