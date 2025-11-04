ab6d0799f72c199c7858b73e4fc8787523a4f538
Ken Chan <chankiyu22@gmail.com>
2025-11-04 16:22:24 +0800

    staking-image: Add base_url to config
    refs likecoin/likecoin-op#262

--- likecollective-staking-position-image/staking_image/config/config.py
+++ likecollective-staking-position-image/staking_image/config/config.py
@@ -1,8 +1,9 @@
-from pydantic import ConfigDict
+from pydantic import ConfigDict, Field, HttpUrl
 from pydantic_settings import BaseSettings
 
 
 class Config(BaseSettings, frozen=True):
+    base_url: HttpUrl = Field(default=HttpUrl("http://localhost:8000"))
     rpc_url: str
     likecoin_contract_address: str
 
